51e08b74612511b9cb556ad735ea3f09702ca986
JaffarShah <sjeffshah@gmail.com>
2019-02-13 19:10:54 +0000

    Added EJS Engine

--- express_server.js
+++ express_server.js
@@ -2,17 +2,24 @@ var express = require("express");
 var app = express();
 var PORT = 8080; // default port 8080
 
+app.set("view engine", "ejs");
+
+// serving public assets
+// serve STATIC files from the filesystem like images, css, javascript, erc.
+
 var urlDatabase = {
   "b2xVn2": "http://www.lighthouselabs.ca",
   "9sm5xK": "http://www.google.com"
 };
 
+// Route HANDLER
 app.get("/", (req, res) => {
-  res.render("urls");
+  // Looks in the filesystem for a file system at ./views/... called urls.ejs (because the view engine is ejs)
+  res.render("urls", { variable: 'Something else' });
 });
 
-app.listen(PORT, () => {
-  console.log(`Example app listening on port ${PORT}!`);
+app.get("/urls/:var/and/:xyz", (req, res) => {
+  res.send(req.params.xyz)
 });
 
 app.get("/urls.json", (req, res) => {
@@ -21,4 +28,9 @@ app.get("/urls.json", (req, res) => {
 
 app.get("/hello", (req, res) => {
   res.send("<html><body>Hello <b>World</b></body></html>\n");
+});
+
+// Starts the server process that listens on the port
+app.listen(PORT, () => {
+  console.log(`Example app listening on port ${PORT}!`);
 });

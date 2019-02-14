b2861835fa8a9e520b5f590dcf2e14948cbccad3
JaffarShah <sjeffshah@gmail.com>
2019-02-14 00:21:08 +0000

    My own commentary in details

--- express_server.js
+++ express_server.js
@@ -1,19 +1,19 @@
-var express = require("express");
+var express = require("express"); //Step 1
 var app = express();
 var PORT = 8080; // default port 8080
 
-app.set("view engine", "ejs");
+app.set("view engine", "ejs"); //Step 2
 
 // serving public assets
 // serve STATIC files from the filesystem like images, css, javascript, erc.
 
-var urlDatabase = {
+var urlDatabase = { //part of Step 1
   "b2xVn2": "http://www.lighthouselabs.ca",
   "9sm5xK": "http://www.google.com"
 };
 
 // Route HANDLER
-app.get("/", (req, res) => {
+app.get("/", (req, res) => { //part of Step 1
   // Looks in the filesystem for a file system at ./views/... called urls.ejs (because the view engine is ejs)
   res.render("urls_index", { variable: 'Something else' });
 });
@@ -22,7 +22,7 @@ app.get("/urls/:var/and/:xyz", (req, res) => {
   res.send(req.params.xyz)
 });
 
-app.get("/urls.json", (req, res) => {
+app.get("/urls.json", (req, res) => { //Step 3 (look for step 4 too), here we are adding a new route. Important: Any change in this file which is the 'server' requires us to restar the server in the cmd.
   res.json(urlDatabase);
 });
 
@@ -42,10 +42,10 @@ app.get("/hello", (req, res) => {
   res.send("<html><body>Hello <b>World</b></body></html>\n");
 });
 
-// Starts the server process that listens on the port
+// part of Step 1. This part starts the server process that listens on the port. This should be written in the bottom to make it look clean and easier to find.
 app.listen(PORT, () => {
   console.log(`Example app listening on port ${PORT}!`);
 });
 
 
-// Make sure every step is tested via the cmd. and chrome: http://localhost:8080/
+// Step 4 and so on: Make sure every step is tested via the cmd. and chrome: http://localhost:8080/
